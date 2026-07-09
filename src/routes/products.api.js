12e8acd0668776af204450e5510a7656dc21c8c2
Soumyajit <soumyajitde302@gmail.com>
2026-07-09 11:28:38 +0530

    v-2

--- src/routes/products.api.js
+++ src/routes/products.api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/ProductController');
 
-// Routes map cleanly to class methods
+
 router.route('/')
   .post(productController.createProduct)
   .get(productController.getAllProducts);
